37eb609494f0ac89aeb89324fa0c27028b514279
wisnuyare <wisnuyare@gmail.com>
2025-02-28 06:52:35 +0700

    add debug log

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -14,6 +14,8 @@ interface AuthState {
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log(API_BASE_URL)
+
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
